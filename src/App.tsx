0a1426c44d0e09611ac5efcf87e9b09a8eea64e5
ilya.markov212001 <ilya.markov212001@gmail.com>
2023-09-06 14:48:37 +0300

    version 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import {QuestionsAnswers} from "./components/questions-answers/questionsAnswers";
 import {Banner} from "./components/banner/banner";
 import {Feature} from "./components/feature/feature";
-import {Task} from "./components/task/task";
 import {Tasks} from "./components/tasks/tasks";
 
 const questionsAnswers = [
@@ -200,160 +199,162 @@ const tasks2 = [
 function App() {
     return (
         <div className="page">
-            <header>
-                <div className="logo">
-                    <img src="https://avatars.mds.yandex.net/get-lpc/1635340/9fa4e259-3ffa-4190-bba6-b4d81cb6c51c/orig"
-                         alt=""/>
-                </div>
-            </header>
-            <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
-                <Banner
-                    title="Мы научились генерировать картинки для ряда рекламных рубрик."
-                    body="Будем предлагать их пользователям на этапе выбора изображений для рекламных материалов.
-                    Ecom Аналитика"
-                    buttons={[buttons[0], buttons[1]]}
-                />
-            </div>
-            <div className="offsets" style={{"--offsets-background-color": "#3287ff"} as React.CSSProperties}>
-                <Banner
-                    title="Аналитика рынка интернет-торговли для продавцов и производителей"
-                    body="Смотрите статистику по динамике продаж, доле брендов, популярным товарам,
-                    ценам и интересам покупателей — в сотнях категорий"
-                    buttons={[buttons[1]]}
-                />
-            </div>
-            <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
-                <div className="carousel">
-                    <div
-                        className="text-block header"
-                        style={{"--font-family": "Arial", "--color": "black", "max-width": "65rem"} as
-                            React.CSSProperties}
-                    >
-                        Что предлагает сервис
-                    </div>
-                    <AliceCarousel
-                        mouseTracking
-                        items={features}
-                        infinite={true}
-                        responsive={{0: {items: 2}}}
-                        disableDotsControls={true}
-                        paddingLeft={10}
-                        paddingRight={10}
-                    />
-                </div>
-            </div>
-
-            <div className="offsets" style={{"--offsets-background-color": "#131720"} as React.CSSProperties}>
-                <Tasks title="Какие задачи помогает решать сервис" tasks={tasks1}/>
-            </div>
-            <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
-                <div className="carousel">
-                    <div className="text-block header" style={{"--font-family": "Arial", "max-width": "65rem"} as
-                        React.CSSProperties}>
-                        Как выглядят отчёты
-                    </div>
-                    <AliceCarousel
-                        mouseTracking
-                        items={reports}
-                        infinite={true}
-                        paddingLeft={20}
-                        paddingRight={20}
-                    />
-                </div>
-            </div>
-
-            <div className="offsets" style={{"--offsets-background-color": "#131720"} as React.CSSProperties}>
-                <Tasks title="Как мы работаем с данными" tasks={tasks2}/>
-            </div>
-            <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
-                <Banner
-                    title="Как получить доступ к данным"
-                    body="Набор доступных данных в каждой категории зависит от вашего объёма
-                    продаж в ней. Подробнее — в Справке. 
-                    Как открыть доступ в личный кабинет:"
-                    buttons={[buttons[1], buttons[2]]}
-                >
-                    <QuestionsAnswers questionsAnswers={answers}/>
-                </Banner>
-            </div>
-            <div className="offsets" style={{"--offsets-background-color": "#3287ff"} as React.CSSProperties}>
-                <div className="carousel">
-                    <div className="text-block header" style={{"--font-family": "Arial", "max-width": "65rem"} as
-                        React.CSSProperties}>
-                        Кейсы партнёров
-                    </div>
-                    <AliceCarousel
-                        mouseTracking
-                        items={reports}
-                        infinite={true}
-                        paddingLeft={20}
-                        paddingRight={20}
-                    />
-                </div>
-            </div>
-            <div className="offsets" style={{"--offsets-background-color": "white"} as React.CSSProperties}>
-                <QuestionsAnswers title="Вопросы и ответы" questionsAnswers={questionsAnswers}/>
-            </div>
-            <footer>
-                <div className="footer-top">
+            <section style={{display:"flex","flex":"1", "flex-direction":"column"} as React.CSSProperties}>
+                <header>
                     <div className="logo">
                         <img src="https://avatars.mds.yandex.net/get-lpc/1635340/9fa4e259-3ffa-4190-bba6-b4d81cb6c51c/orig"
                              alt=""/>
                     </div>
+                </header>
+                <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
+                    <Banner
+                        title="Мы научились генерировать картинки для ряда рекламных рубрик."
+                        body="Будем предлагать их пользователям на этапе выбора изображений для рекламных материалов.
+                    Ecom Аналитика"
+                        buttons={[buttons[0], buttons[1]]}
+                    />
                 </div>
-                <div className="footer-bottom">
-                    <div className="footer-left">
-                        <nav className="navigation-menu">
-                            <ul className="navigation-menu__wrapper">
-                                <li className="navigation-menu__item">
-                                    <a
-                                        className="text-block"
-                                        href="https://partner.market.yandex.ru/welcome/"
-                                        target="_blank"
-                                        rel="noopener"
-                                        style={{"--color": "#81899A", "--text-decoration": "none",
-                                            "--font-family": "Arial",} as React.CSSProperties}
-                                    >
-                                        Сайт для партнёров
-                                    </a>
-                                </li>
-                                <li className="navigation-menu__item">
-                                    <a
-                                        className="text-block"
-                                        href="https://yandex.ru/legal/confidential/"
-                                        target="_blank"
-                                        rel="noopener"
-                                        style={{"--color": "#81899A", "--text-decoration": "none",
-                                            "--font-family": "Arial",} as React.CSSProperties}
-                                    >
-                                        Политика конфиденциальности
-                                    </a>
-                                </li>
-                                <li className="navigation-menu__item">
-                                    <a
-                                        className="text-block"
-                                        href="https://forms.yandex.ru/surveys/13473709.a3a02c08965ffcb86ee9ff745a97fd464cffb2d4/"
-                                        target="_blank"
-                                        rel="noopener"
-                                        style={{"--color": "#81899A", "--text-decoration": "none",
-                                            "--font-family": "Arial",} as React.CSSProperties}
-                                    >
-                                        Задать вопрос
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className="footer-right">
-                        <p className="text-block"
-                           style={{"--color": "#81899A", "--text-decoration": "none",
-                               "--font-family": "Arial",} as React.CSSProperties}
+                <div className="offsets" style={{"--offsets-background-color": "#3287ff"} as React.CSSProperties}>
+                    <Banner
+                        title="Аналитика рынка интернет-торговли для продавцов и производителей"
+                        body="Смотрите статистику по динамике продаж, доле брендов, популярным товарам,
+                    ценам и интересам покупателей — в сотнях категорий"
+                        buttons={[buttons[1]]}
+                    />
+                </div>
+                <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
+                    <div className="carousel">
+                        <div
+                            className="text-block header"
+                            style={{"--font-family": "Arial", "--color": "black", "max-width": "65rem"} as
+                                React.CSSProperties}
                         >
-                            ©2023 ООО «Яндекс»
-                        </p>
+                            Что предлагает сервис
+                        </div>
+                        <AliceCarousel
+                            mouseTracking
+                            items={features}
+                            infinite={true}
+                            responsive={{0: {items: 2}}}
+                            disableDotsControls={true}
+                            paddingLeft={10}
+                            paddingRight={10}
+                        />
                     </div>
                 </div>
-            </footer>
+
+                <div className="offsets" style={{"--offsets-background-color": "#131720"} as React.CSSProperties}>
+                    <Tasks title="Какие задачи помогает решать сервис" tasks={tasks1}/>
+                </div>
+                <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
+                    <div className="carousel">
+                        <div className="text-block header" style={{"--font-family": "Arial", "max-width": "65rem"} as
+                            React.CSSProperties}>
+                            Как выглядят отчёты
+                        </div>
+                        <AliceCarousel
+                            mouseTracking
+                            items={reports}
+                            infinite={true}
+                            paddingLeft={20}
+                            paddingRight={20}
+                        />
+                    </div>
+                </div>
+
+                <div className="offsets" style={{"--offsets-background-color": "#131720"} as React.CSSProperties}>
+                    <Tasks title="Как мы работаем с данными" tasks={tasks2}/>
+                </div>
+                <div className="offsets" style={{"--offsets-background-color": "#f4f7fa"} as React.CSSProperties}>
+                    <Banner
+                        title="Как получить доступ к данным"
+                        body="Набор доступных данных в каждой категории зависит от вашего объёма
+                    продаж в ней. Подробнее — в Справке. 
+                    Как открыть доступ в личный кабинет:"
+                        buttons={[buttons[1], buttons[2]]}
+                    >
+                        <QuestionsAnswers questionsAnswers={answers}/>
+                    </Banner>
+                </div>
+                <div className="offsets" style={{"--offsets-background-color": "#3287ff"} as React.CSSProperties}>
+                    <div className="carousel">
+                        <div className="text-block header" style={{"--font-family": "Arial", "max-width": "65rem"} as
+                            React.CSSProperties}>
+                            Кейсы партнёров
+                        </div>
+                        <AliceCarousel
+                            mouseTracking
+                            items={reports}
+                            infinite={true}
+                            paddingLeft={20}
+                            paddingRight={20}
+                        />
+                    </div>
+                </div>
+                <div className="offsets" style={{"--offsets-background-color": "white"} as React.CSSProperties}>
+                    <QuestionsAnswers title="Вопросы и ответы" questionsAnswers={questionsAnswers}/>
+                </div>
+                <footer>
+                    <div className="footer-top">
+                        <div className="logo">
+                            <img src="https://avatars.mds.yandex.net/get-lpc/1635340/9fa4e259-3ffa-4190-bba6-b4d81cb6c51c/orig"
+                                 alt=""/>
+                        </div>
+                    </div>
+                    <div className="footer-bottom">
+                        <div className="footer-left">
+                            <nav className="navigation-menu">
+                                <ul className="navigation-menu__wrapper">
+                                    <li className="navigation-menu__item">
+                                        <a
+                                            className="text-block"
+                                            href="https://partner.market.yandex.ru/welcome/"
+                                            target="_blank"
+                                            rel="noopener"
+                                            style={{"--color": "#81899A", "--text-decoration": "none",
+                                                "--font-family": "Arial",} as React.CSSProperties}
+                                        >
+                                            Сайт для партнёров
+                                        </a>
+                                    </li>
+                                    <li className="navigation-menu__item">
+                                        <a
+                                            className="text-block"
+                                            href="https://yandex.ru/legal/confidential/"
+                                            target="_blank"
+                                            rel="noopener"
+                                            style={{"--color": "#81899A", "--text-decoration": "none",
+                                                "--font-family": "Arial",} as React.CSSProperties}
+                                        >
+                                            Политика конфиденциальности
+                                        </a>
+                                    </li>
+                                    <li className="navigation-menu__item">
+                                        <a
+                                            className="text-block"
+                                            href="https://forms.yandex.ru/surveys/13473709.a3a02c08965ffcb86ee9ff745a97fd464cffb2d4/"
+                                            target="_blank"
+                                            rel="noopener"
+                                            style={{"--color": "#81899A", "--text-decoration": "none",
+                                                "--font-family": "Arial",} as React.CSSProperties}
+                                        >
+                                            Задать вопрос
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="footer-right">
+                            <p className="text-block"
+                               style={{"--color": "#81899A", "--text-decoration": "none",
+                                   "--font-family": "Arial",} as React.CSSProperties}
+                            >
+                                ©2023 ООО «Яндекс»
+                            </p>
+                        </div>
+                    </div>
+                </footer>
+            </section>
 
         </div>
     );
